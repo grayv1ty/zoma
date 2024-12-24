aa7165f7652111a1e5081ddd7c350af4dce4977d
uguumur <uguumur.oldokh@gmail.com>
2024-12-23 18:17:47 -0600

    chore: fixed UI layout and icon

--- src/components/root/Alert.tsx
+++ src/components/root/Alert.tsx
@@ -2,28 +2,27 @@ import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent } from "../ui/card";
 import { Button } from "../ui/button";
+import { ReactNode } from "react";
 
 interface PermissionCardProps {
   title: string;
-  iconUrl?: string;
+  icon?: ReactNode;
 }
 
-const Alert = ({ title, iconUrl }: PermissionCardProps) => {
+const Alert = ({ title, icon }: PermissionCardProps) => {
   return (
     <section className="flex items-center justify-center h-screen w-full">
       <Card className="w-full max-w-[520px] border-none bg-dark-1 p-6 py-9 text-white">
         <CardContent>
           <div className="flex flex-col gap-9">
             <div className="flex flex-col gap-3.5">
-              {iconUrl && (
-                <div className="flex-center">
-                  <Image src={iconUrl} width={72} height={72} alt="icon" />
-                </div>
+              {icon && (
+                <div className="flex items-center justify-center">{icon}</div>
               )}
               <p className="text-center text-xl font-semibold">{title}</p>
             </div>
 
-            <Button asChild className="bg-blue-1">
+            <Button asChild className="bg-blue-500">
               <Link href="/">Back to Home</Link>
             </Button>
           </div>
