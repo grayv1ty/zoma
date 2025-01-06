e1d5302f59b55fc92d296e5ce4e73e644fa6d331
uguumur <uguumur.oldokh@gmail.com>
2025-01-06 10:31:52 -0600

    chore(style): added padding in MeetingSetup page

--- src/components/meeting/MeetingSetup.tsx
+++ src/components/meeting/MeetingSetup.tsx
@@ -173,7 +173,7 @@ const MeetingSetup = ({
     );
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+    <div className="flex py-4 min-h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-center text-2xl font-bold">Setup</h1>
       <VideoPreview />
       <div className="flex h-16 items-center justify-center gap-3">
